2b0436b86a615d45994fb6e32de1399422d29b4c
GeoS74 <gsirotkin@yandex.ru>
2022-08-18 19:07:02 +0500

    develop title and bold tag handlers

--- class/Converter.ts
+++ class/Converter.ts
@@ -1,6 +1,55 @@
 export class Converter {
+  regexp: {
+    title: RegExp,
+    bold: RegExp,
+  } = {
+      title: /#+\s/,
+      bold: /__/,
+    };
+
   markdownToHTML(markdown: string): string {
-    console.log(markdown);
-    return '';
+    const blocks: string[] = this.parse(markdown);
+    this.addTagsHTML(blocks);
+
+    console.log(blocks);
+
+    return this.getHTML(blocks);
+  }
+
+  private addTagsHTML(blocks: string[]): void {
+    for (const k in blocks) {
+      blocks[k] = this.title(blocks[k]);
+      blocks[k] = this.bold(blocks[k]);
+    }
+  }
+
+  private bold(block: string): string {
+    const matched: RegExpMatchArray = block.match(this.regexp.bold) || [];
+
+    if (!matched?.input) {
+      return block;
+    }
+    console.log(matched);
+
+    return block;
+  }
+
+  private title(block: string): string {
+    const matched: RegExpMatchArray = block.match(this.regexp.title) || [];
+
+    if (!matched?.input) {
+      return block;
+    }
+
+    const str: string = matched.input.slice(matched[0].length);
+    return `<h${matched[0].length - 1}>${str}</h${matched[0].length - 1}>`;
+  }
+
+  private getHTML(blocks: string[]): string {
+    return blocks.join('\n\n');
+  }
+
+  private parse(markdown: string): string[] {
+    return markdown.split('\n\n');
   }
 }
--- index.ts
+++ index.ts
@@ -5,52 +5,53 @@ import * as fs from 'fs';
 import { Converter } from './class/Converter';
 
 const server: http.Server = http.createServer();
+const converter: Converter = new Converter();
 
-function _getFileName(path: string): string {
-  return `${path.split('/')[1] || 'main'}.md`;
-}
-
-server.on('request', (request: http.IncomingMessage, response: http.ServerResponse): void => {
+server.on('request', async (request: http.IncomingMessage, response: http.ServerResponse): Promise<void> => {
   try {
     if (request.method !== 'GET') {
       response.statusCode = 404;
       response.end('not found');
       return;
     }
-  
-    if (!request?.url) {
-      return;
+
+    if (request?.url) {
+      const url = new URL(request.url, 'http://localhost:3500');
+      const fname: string = _getFileName(url.pathname);
+      const html = await _readFile(fname);
+
+      response.end(html);
     }
-  
-    const url = new URL(request.url, 'http://localhost:3500');
-    response.end(_getFileName(url.pathname));
-    
-  } catch(error: unknown){
-    if(error instanceof Error){
-      console.log(error.message)
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.message);
     }
+
+    response.statusCode = 500;
+    response.end('internal server error');
   }
 });
 
-server.listen(3500, (): void => {
-  console.log('server run at 3500 port');
-});
+server.listen(3500, (): void => console.log('server run at 3500 port'));
 
-// const converter: Converter = new Converter();
-// const filePath: string = path.join(__dirname, '../../wiki/testpage.md');
+function _readFile(fname: string): Promise<string | void> {
+  const filePath: string = path.join(__dirname, `../../wiki/${fname}`);
 
-// new Promise((resolve: (value: string) => void, reject: (error: NodeJS.ErrnoException) => void): void => {
-//   fs.readFile(filePath, (error: NodeJS.ErrnoException | null, buff: Buffer) => {
-//     if (error) {
-//       reject(error);
-//       return;
-//     }
-//     resolve(buff.toString())
-//   })
-// })
-//   .then((markdown: string): string => {
-//     return converter.markdownToHTML(markdown)
-//   })
-//   .catch((error: NodeJS.ErrnoException): void => {
-//     console.log(error.message);
-//   });
+  return new Promise((resolve: (value: string) => void, reject: (error: NodeJS.ErrnoException) => void): void => {
+    fs.readFile(filePath, (error: NodeJS.ErrnoException | null, buff: Buffer) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(buff.toString());
+    });
+  })
+    .then((markdown: string): string => converter.markdownToHTML(markdown))
+    .catch((error: NodeJS.ErrnoException): void => {
+      throw error;
+    });
+}
+
+function _getFileName(path: string): string {
+  return `${decodeURI(path).split('/')[1] || 'main'}.md`;
+}
