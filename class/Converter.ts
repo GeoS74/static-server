129c793cd1283d7fa08b1965dc19f48ed61bd99a
GeoS74 <gsirotkin@yandex.ru>
2022-08-19 16:32:50 +0500

    debug link

--- class/Converter.ts
+++ class/Converter.ts
@@ -3,47 +3,69 @@
 export class Converter {
   regexp: {
     title: RegExp,
+    internalLink: RegExp,
     bold: RegExp,
   } = {
       title: /#+\s/,
+      internalLink: /\[\[[^\[\]]+\]\]/g,
       bold: /__/,
     };
 
   markdownToHTML(markdown: string): string {
     const html: string[] = [];
-    
-    for(const block in markdown.split(`\n\n`)){
+
+    for(const block of markdown.split(`\n\n`)){
       const divs: string[] = [];
 
-      for(const line of markdown.split(`\n`)){
+      for(const line of block.split(`\n`)){
         divs.push(this.lineProcessing(line));
       }
       html.push(divs.join('\n'));
     }
-
     return html.join('\n\n');
   }
 
   private lineProcessing(line: string): string {
+    line = this.internalLink(line);
     line = this.title(line);
     return line;
   }
 
-  private bold(block: string): string {
-    const matched: RegExpMatchArray = block.match(this.regexp.bold) || [];
+  private bold(line: string): string {
+    return line;
+  }
 
-    if (!matched?.input) {
-      return block;
+  private internalLink(line: string): string {
+    const iterator: IterableIterator<RegExpMatchArray> = line.matchAll(this.regexp.internalLink);
+    const matched: RegExpMatchArray[] = [...iterator];
+
+    if (!matched.length) {
+      return line;
     }
-    console.log(matched);
+    
+    for(const title of matched){
+      const newTitle = title[0].slice(2, -2);
+      const link = `<a href="/${newTitle}">${newTitle}</a>`;
+      line = line.replace(title[0], link);
 
-    return block;
+      console.log(line);
+    console.log(title[0]);
+    console.log(link);
+    }
+
+     
+
+    return line;
+  }
+
+  private externalLink(line: string): string {
+    return line;
   }
 
   private title(line: string): string {
     const matched: RegExpMatchArray = line.match(this.regexp.title) || [];
 
-    if (!matched?.input) {
+    if (!matched.length) {
       return line;
     }
     return line.replace(matched[0], `<h${matched[0].length-1}>`) + `</h${matched[0].length-1}>`;
